b203b3daae99725e551ddbe8551f938e013d6a58
YXUNGGG <s.krivostanenko@mail.ru>
2026-09-08 16:15:26 +0500

    refactor: get rid of cascading renders triggerring by useEffect hook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,8 +15,7 @@ const eslintConfig = defineConfig([
   ]),
   {
     rules: {
-      "react-hooks/exhaustive-deps": "off",
-      "react-hooks/set-state-in-effect": "off"
+      "react-hooks/exhaustive-deps": "off"
     }
   }
 ]);
--- src/components/picture-card.tsx
+++ src/components/picture-card.tsx
@@ -1,34 +1,30 @@
 import { clsx } from "cn";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 type PictureCardProps = {
   showNextPicture: boolean;
 };
 
 export function PictureCard({ showNextPicture }: PictureCardProps) {
-  const [isMounted, setIsMounted] = useState(false);
+  const [hasShown, setHasShown] = useState(false);
+  if (showNextPicture && !hasShown) setHasShown(true);
 
   const picBaseStyles = "h-80 sm:h-132 bg-center bg-size-[auto_103%] rounded-xl border shadow-lg";
-
-  useEffect(() => {
-    if (showNextPicture) setIsMounted(true);
-  }, [showNextPicture]);
 
   return (
     <div className="w-full max-w-132 h-full relative">
       <div className={clsx(picBaseStyles, "bg-[url(/pic-1.png)]")} />
-      {isMounted && (
-        <div
-          onAnimationEnd={() => {
-            if (!showNextPicture) setIsMounted(false);
-          }}
-          className={clsx(
-            picBaseStyles,
-            showNextPicture ? "animate-spin-in" : "animate-spin-out",
-            "absolute inset-0 bg-cover bg-[url(/pic-2.png)]"
-          )}
-        />
-      )}
+      <div
+        className={clsx(
+          picBaseStyles,
+          "absolute inset-0 bg-cover bg-[url(/pic-2.png)]",
+          // I came back to refactor cascading render issue with useEffect,
+          // but i decided to even remove conditional rendering to let you see pic-2 immediately (without loading).
+          // But if it were a real case and the component were compicated, I'd think to keep the cond. render.
+          // cause user may left on booking process and pic-2 won't be useful in DOM
+          showNextPicture ? "animate-spin-in" : hasShown ? "animate-spin-out" : "opacity-0"
+        )}
+      />
     </div>
   );
 }
